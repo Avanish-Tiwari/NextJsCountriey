0968f3a1c9705cd536d4b7041c0d514c1dbe0657
Avanish <avanishtiwari@outlook.in>
2025-10-24 02:20:42 +0530

    filter feature implemented

--- src/app/ui/Filter.jsx
+++ src/app/ui/Filter.jsx
@@ -1,6 +1,29 @@
-export default function Filter() {
-  return <div>Filter by Region</div>;
+export default function Filter({ handleRegion, selectedRegion }) {
+  const regions = [
+    "Europe",
+    "Americas",
+    "Africa",
+    "Asia",
+    "Oceania",
+    "Antarctic",
+  ];
+  return (
+    <div className="flex item-center gap-2">
+      <label htmlFor="region" className="font-medium">
+        Filter by Region
+      </label>
+      <select
+        id="region"
+        value={selectedRegion || ""}
+        onChange={(e) => handleRegion(e.target.value || null)}
+      >
+        <option value="">Select</option>
+        {regions.map((region) => (
+          <option key={region} value={region}>
+            {region}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
-
-
-
